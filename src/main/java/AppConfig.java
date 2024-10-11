aa7397bb763809aa9c1d904dccd6e6252c0490b1
sworeman <sworeman@yandex.ru>
2024-10-11 19:00:23 +0300

    initial commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getName() {
+        Cat cat = new Cat();
+        cat.setName("Lucifer");
+        return cat;
+    }
 }
